0ec5f26b7e6d17abe6cb8e8237438f569d24722c
Drew2134 <awinchell@cobec.com>
2023-03-29 22:33:19 -0500

    project2 startup

--- js/main.js
+++ js/main.js
@@ -72,6 +72,36 @@ require([
     $(document).ready(() => {
         $("#splash")[0].open = true;
     });
+    $(document).ready(function(){
+
+        var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        var isPortrait = viewportHeight > viewportWidth;
+
+        $( window ).resize(onresize);
+
+        function onresize() {
+            var newViewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+            var newViewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+            var hasOrientationChanged = (newViewportHeight > newViewportWidth) != isPortrait;
+            var addressbarHeight = 130;
+
+            if (!hasOrientationChanged && (newViewportHeight != viewportHeight)) {
+                addressbarHeight = Math.abs(newViewportHeight - viewportHeight);
+                if (newViewportHeight < viewportHeight) {
+                    // Android Chrome address bar has appeared
+                } else {
+                    // Android Chrome address bar has disappeared
+                }
+            } else if(hasOrientationChanged) {
+                // Orientation change
+            }
+
+            viewportHeight = newViewportHeight;
+            viewportWidth = newViewportWidth;
+            isPortrait = viewportHeight > viewportWidth;
+        }
+    });
 
     function switchView () {
         const is3D = appConfig.activeView.type === "3d";
